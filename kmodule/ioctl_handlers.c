ffba66f5fb870d7f9a39b8fa73f80778b8b5ba9c
Ilia <Rubiliam2003@gmail.com>
2024-08-02 21:23:26 -0700

    Connect IV generation and key derivation to ioctl_handlers.c

--- kmodule/ioctl_handlers.c
+++ kmodule/ioctl_handlers.c
@@ -3,18 +3,27 @@
 #include <linux/uaccess.h>
 #include "ioctl_defines.h"
 #include "bio_operations.h"
+#include "key_derivation.h"
+#include "iv.h"
+#include "encryption.h"
 
 static long usb_encryption_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     char buffer[512];
-    sector_t sector = 0; // For simplicity, sector is hardcoded here
-    
+    sector_t sector = 0;
+    u8 derived_key[32];
+    u8 iv[16];
+
     switch (cmd) {
         case IOCTL_READ:
+            generate_iv(sector, derived_key, iv);  // Generate IV for block
+            aes_decrypt(buffer, buffer, derived_key, iv); // Decrypt
             if (usb_bio_read(NULL, sector, buffer) == 0) {
                 printk(KERN_INFO "USB Encryption Layer: IOCTL_READ successful\n");
             }
             break;
         case IOCTL_WRITE:
+            generate_iv(sector, derived_key, iv);  // Generate IV for block
+            aes_encrypt(buffer, buffer, derived_key, iv); // Encrypt
             if (usb_bio_write(NULL, sector, buffer) == 0) {
                 printk(KERN_INFO "USB Encryption Layer: IOCTL_WRITE successful\n");
             }
