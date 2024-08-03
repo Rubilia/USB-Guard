73c3b8ca6267553fa7304f56ca02b9707b85871a
Ilia <Rubiliam2003@gmail.com>
2024-08-03 16:04:04 -0700

    Add test_write.c and test_read.c for basic encryption testing

---
+++ test/test_read.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include "common.h"
+
+int main() {
+    int fd = open("/dev/kmod", O_RDWR);
+    if (fd < 0) {
+        printf("Error opening device\n");
+        return -1;
+    }
+
+    char buffer[512];
+    ioctl(fd, IOCTL_READ, buffer);
+    printf("Read data: %s\n", buffer);
+    
+    close(fd);
+    return 0;
+}
---
+++ test/test_write.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include "common.h"
+
+int main() {
+    int fd = open("/dev/kmod", O_RDWR);
+    if (fd < 0) {
+        printf("Error opening device\n");
+        return -1;
+    }
+
+    char data[512] = "Hello, USB Encryption Layer!";
+    ioctl(fd, IOCTL_WRITE, data);
+    
+    close(fd);
+    return 0;
+}
